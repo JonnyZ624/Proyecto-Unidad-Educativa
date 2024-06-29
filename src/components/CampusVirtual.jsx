a81c72d15d9060b50c0c79bbea3e5e1f1a394497
andreeeli88 <andreeli10@hotmail.com>
2024-06-29 16:37:55 -0500

    AHORA SI

--- src/components/CampusVirtual.jsx
+++ src/components/CampusVirtual.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import { Button } from 'react-bootstrap'
 import BibliotecaVirtual from './CampusScreens/BibliotecaVirtual'
+import AulaVirtual from './CampusScreens/AulaVirtual'
+import Soporte from './CampusScreens/Soporte'
+import RecordAcademico from './CampusScreens/RecordAcademico'
+import Perfiles from './CampusScreens/Perfiles'
 
 function CampusVirtual() {
   return (
     <div>
       <Button className='btn-campus'onClick={BibliotecaVirtual}>Biblioteca Virtual</Button>
-      <Button className='btn-campus'>Aula Virtual</Button>
-      <Button className='btn-campus'>Soporte y Ayuda</Button>
-      <Button className='btn-campus'>Recod Académico</Button>
-      <Button className='btn-campus'>Perfiles</Button>
+      <Button className='btn-campus' onClick={AulaVirtual}>Aula Virtual</Button>
+      <Button className='btn-campus' onClick={Soporte} >Soporte y Ayuda</Button>
+      <Button className='btn-campus' onClick={RecordAcademico}>Record Académico</Button>
+      <Button className='btn-campus' onClick={Perfiles} >Perfiles</Button>
     </div>
   )
 }
