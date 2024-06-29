792bba450001c3375b1bd06651ab9b7f60aba780
J4m3s465 <jamesduran@outlook.com>
2024-06-29 17:07:20 -0500

    Campus 2

--- src/components/CampusScreens/BibliotecaVirtual.jsx
+++ src/components/CampusScreens/BibliotecaVirtual.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import '../style/CampusVirtual.css'
 
 export default function BibliotecaVirtual() {
   return (
-    <div>BibliotecaVirtual</div>
+    <div className="campus-virtual-container">
+    <h1 className="campus-virtual-title">Biblioteca Virtual</h1>
+    <Link to="/campusVirtual" className="campus-virtual-link">
+      <Button className="btn-campus">Volver al Campus Virtual</Button>
+    </Link>
+  </div>
+    
   )
 }
+
--- src/components/CampusScreens/RecordAcademico.jsx
+++ src/components/CampusScreens/RecordAcademico.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import '../style/CampusVirtual.css'
 
 export default function RecordAcademico() {
   return (
-    <div>RecordAcademico</div>
+    <div className="campus-virtual-container">
+    <h1 className="campus-virtual-title">Record Academico</h1>
+    <Link to="/campusVirtual" className="campus-virtual-link">
+      <Button className="btn-campus">Volver al Campus Virtual</Button>
+    </Link>
+  </div>
+    
   )
 }
